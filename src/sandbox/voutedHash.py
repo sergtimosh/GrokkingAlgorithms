162998390e8e44b6d9c8f2d20e9e52336baf3f61
serhiytymoshenko <sergtimosh@gmail.com>
2019-12-02 16:35:25 +0200

    add Hash usage function

---
+++ src/sandbox/voutedHash.py
@@ -0,0 +1,10 @@
+voted = {}
+def check_voter(name):
+    if voted.get(name):
+        print("kick them out!")
+    else:
+        voted[name] = True
+        print("let them vote!")
+
+check_voter("Tom")
+check_voter("Tom")
